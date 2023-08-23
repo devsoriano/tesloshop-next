21dd1bd4e3ea6c96d3cb8c666ae12a4228e61658
Rafael Soriano Ramírez <rafa.soriano.ramirez@gmail.com>
2023-08-22 22:07:05 -0600

    [Add]Avance pantallas

---
+++ components/products/SizeSelector.tsx
@@ -0,0 +1,24 @@
+import { FC } from "react";
+import { ISize } from "../../interfaces";
+import { Box, Button } from "@mui/material";
+
+interface Props {
+  selectSize?: ISize;
+  sizes: ISize[];
+}
+
+export const SizeSelector: FC<Props> = ({ selectSize, sizes }) => {
+  return (
+    <Box>
+      {sizes.map((size) => (
+        <Button
+          key={size}
+          size="small"
+          color={selectSize === size ? "primary" : "info"}
+        >
+          {size}
+        </Button>
+      ))}
+    </Box>
+  );
+};
